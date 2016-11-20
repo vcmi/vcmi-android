b63c442c6a9a0fa4d16c639e62264a1cefcf6c91
Fay <no.email@at.all>
2016-11-20 18:12:29 +0100

    Added iconv as a submodule; Cleanup;

--- ext/iconv/build.mk
+++ ext/iconv/build.mk
@@ -1,2 +1,25 @@
-TMP_ICONV_DIR := $(call my-dir)
-include $(TMP_ICONV_DIR)/code/jni/Android.mk
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS) 
+ 
+LOCAL_MODULE    := iconv 
+ 
+LOCAL_CFLAGS    := \
+	-Wno-multichar \
+	-D_ANDROID \
+	-DBUILDING_LIBICONV \
+	-DIN_LIBRARY \
+	-DLIBDIR="\"c\"" \
+    -I$(LOCAL_PATH)/code/ \
+    -I$(LOCAL_PATH)/code/include/ \
+    -I$(LOCAL_PATH)/code/lib/ \
+	-I$(LOCAL_PATH)/code/libcharset/include \
+
+LOCAL_SRC_FILES := \
+     ./code/lib/iconv.c \
+     ./code/lib/relocatable.c \
+     ./code/libcharset/lib/localcharset.c \
+
+include $(BUILD_SHARED_LIBRARY) 
+ 
+LOCAL_LDLIBS    := -llog -lcharset
