ad8688a2bce76b2626cf066868c326236aea5ea6
Fay <no.email@at.all>
2017-05-14 13:49:47 +0200

    Added sound/music settings inside launcher + minor config loading/saving fixes; Minor launcher refactor;

--- project/vcmi-app/src/main/java/eu/vcmi/vcmi/ActivityLauncher.java
+++ project/vcmi-app/src/main/java/eu/vcmi/vcmi/ActivityLauncher.java
@@ -26,9 +26,11 @@ import eu.vcmi.vcmi.settings.CodepageSettingController;
 import eu.vcmi.vcmi.settings.DoubleConfig;
 import eu.vcmi.vcmi.settings.LauncherSettingController;
 import eu.vcmi.vcmi.settings.ModsBtnController;
+import eu.vcmi.vcmi.settings.MusicSettingController;
 import eu.vcmi.vcmi.settings.PointerModeSettingController;
 import eu.vcmi.vcmi.settings.PointerMultiplierSettingController;
 import eu.vcmi.vcmi.settings.ScreenResSettingController;
+import eu.vcmi.vcmi.settings.SoundSettingController;
 import eu.vcmi.vcmi.settings.StartGameController;
 import eu.vcmi.vcmi.util.FileUtil;
 import eu.vcmi.vcmi.util.Log;
@@ -38,7 +40,7 @@ import eu.vcmi.vcmi.util.SharedPrefs;
 /**
  * @author F
  */
-public class ActivityLauncher extends ActivityBase implements AsyncLauncherInitialization.ILauncherCallbacks
+public class ActivityLauncher extends ActivityBase
 {
     private View mProgress;
     private TextView mErrorMessage;
@@ -47,8 +49,43 @@ public class ActivityLauncher extends ActivityBase implements AsyncLauncherIniti
     private LauncherSettingController<String, Config> mCtrlCodepage;
     private LauncherSettingController<PointerModeSettingController.PointerMode, DoubleConfig> mCtrlPointerMode;
     private LauncherSettingController<Void, Void> mCtrlStart;
-    private List<LauncherSettingController<?, ?>> mActualSettings = new ArrayList<>();
     private LauncherSettingController<Float, SharedPrefs> mCtrlPointerMulti;
+    private LauncherSettingController<Integer, Config> mCtrlSoundVol;
+    private LauncherSettingController<Integer, Config> mCtrlMusicVol;
+    private final List<LauncherSettingController<?, ?>> mActualSettings = new ArrayList<>();
+    private final AsyncLauncherInitialization.ILauncherCallbacks mInitCallbacks = new AsyncLauncherInitialization.ILauncherCallbacks()
+    {
+        @Override
+        public Activity ctx()
+        {
+            return ActivityLauncher.this;
+        }
+
+        @Override
+        public SharedPrefs prefs()
+        {
+            return mPrefs;
+        }
+
+        @Override
+        public void onInitSuccess()
+        {
+            loadConfigFile();
+            mCtrlStart.show();
+            mErrorMessage.setVisibility(View.GONE);
+            mProgress.setVisibility(View.GONE);
+        }
+
+        @Override
+        public void onInitFailure(final AsyncLauncherInitialization.InitResult result)
+        {
+            if (result.mFailSilently)
+            {
+                return;
+            }
+            ActivityLauncher.this.onInitFailure(result, false);
+        }
+    };
 
     @Override
     public void onCreate(final Bundle savedInstanceState)
@@ -70,7 +107,14 @@ public class ActivityLauncher extends ActivityBase implements AsyncLauncherIniti
 
         ((TextView) findViewById(R.id.launcher_version_info)).setText(getString(R.string.launcher_version, BuildConfig.VERSION_NAME));
         initSettingsGui();
-        new AsyncLauncherInitialization(this).execute((Void) null);
+        new AsyncLauncherInitialization(mInitCallbacks).execute((Void) null);
+    }
+
+    @Override
+    public void onBackPressed()
+    {
+        saveConfig();
+        super.onBackPressed();
     }
 
     private void initSettingsGui()
@@ -82,18 +126,33 @@ public class ActivityLauncher extends ActivityBase implements AsyncLauncherIniti
         mCtrlCodepage = new CodepageSettingController(this).init(R.id.launcher_btn_cp, mConfig);
         mCtrlPointerMode = new PointerModeSettingController(this).init(R.id.launcher_btn_pointer_mode, new DoubleConfig(mConfig, mPrefs));
         mCtrlPointerMulti = new PointerMultiplierSettingController(this).init(R.id.launcher_btn_pointer_multi, mPrefs);
+        mCtrlSoundVol = new SoundSettingController(this).init(R.id.launcher_btn_volume_sound, mConfig);
+        mCtrlMusicVol = new MusicSettingController(this).init(R.id.launcher_btn_volume_music, mConfig);
 
         mActualSettings.clear();
         mActualSettings.add(mCtrlCodepage);
         mActualSettings.add(mCtrlScreenRes);
         mActualSettings.add(mCtrlPointerMode);
         mActualSettings.add(mCtrlPointerMulti);
+        mActualSettings.add(mCtrlSoundVol);
+        mActualSettings.add(mCtrlMusicVol);
 
         mCtrlStart.hide();
     }
 
     private void onLaunchGameBtnPressed()
     {
+        saveConfig();
+        startActivity(new Intent(ActivityLauncher.this, SDLActivity.class));
+    }
+
+    private void saveConfig()
+    {
+        if (mConfig == null)
+        {
+            return;
+        }
+
         try
         {
             mConfig.save(new File(FileUtil.configFileLocation()));
@@ -102,7 +161,6 @@ public class ActivityLauncher extends ActivityBase implements AsyncLauncherIniti
         {
             Toast.makeText(this, getString(R.string.launcher_error_config_saving_failed, e.getMessage()), Toast.LENGTH_LONG).show();
         }
-        startActivity(new Intent(ActivityLauncher.this, SDLActivity.class));
     }
 
 
@@ -124,24 +182,21 @@ public class ActivityLauncher extends ActivityBase implements AsyncLauncherIniti
 
     private void onConfigUpdated()
     {
-        if (mCtrlScreenRes != null)
-        {
-            mCtrlScreenRes.updateConfig(mConfig);
-        }
-        if (mCtrlCodepage != null)
-        {
-            mCtrlCodepage.updateConfig(mConfig);
-        }
-        if (mCtrlPointerMode != null)
-        {
-            mCtrlPointerMode.updateConfig(new DoubleConfig(mConfig, mPrefs));
-        }
-        if (mCtrlPointerMulti != null)
-        {
-            mCtrlPointerMulti.updateConfig(mPrefs);
-        }
+        updateCtrlConfig(mCtrlScreenRes, mConfig);
+        updateCtrlConfig(mCtrlCodepage, mConfig);
+        updateCtrlConfig(mCtrlPointerMode, new DoubleConfig(mConfig, mPrefs));
+        updateCtrlConfig(mCtrlPointerMulti, mPrefs);
+        updateCtrlConfig(mCtrlSoundVol, mConfig);
+        updateCtrlConfig(mCtrlMusicVol, mConfig);
     }
 
+    private <TSetting, TConf> void updateCtrlConfig(final LauncherSettingController<TSetting, TConf> ctrl, final TConf config)
+    {
+        if (ctrl != null)
+        {
+            ctrl.updateConfig(config);
+        }
+    }
 
     @Override
     public void onRequestPermissionsResult(final int requestCode, @NonNull final String[] permissions, @NonNull final int[] grantResults)
@@ -150,7 +205,7 @@ public class ActivityLauncher extends ActivityBase implements AsyncLauncherIniti
         {
             if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED)
             {
-                new AsyncLauncherInitialization(this).execute((Void) null); // retry init (we still need to check folders)
+                new AsyncLauncherInitialization(mInitCallbacks).execute((Void) null); // retry init (we still need to check folders)
             }
             else
             {
@@ -159,37 +214,6 @@ public class ActivityLauncher extends ActivityBase implements AsyncLauncherIniti
             return;
         }
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-    }
-
-    @Override
-    public Activity ctx()
-    {
-        return this;
-    }
-
-    @Override
-    public SharedPrefs prefs()
-    {
-        return mPrefs;
-    }
-
-    @Override
-    public void onInitSuccess()
-    {
-        loadConfigFile();
-        mCtrlStart.show();
-        mErrorMessage.setVisibility(View.GONE);
-        mProgress.setVisibility(View.GONE);
-    }
-
-    @Override
-    public void onInitFailure(final AsyncLauncherInitialization.InitResult result)
-    {
-        if (result.mFailSilently)
-        {
-            return;
-        }
-        onInitFailure(result, false);
     }
 
     private void onInitFailure(final AsyncLauncherInitialization.InitResult initResult, final boolean disableAccess)
