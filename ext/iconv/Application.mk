158ba61d3eda2eea06d87f9a224db16439ee5c5e
Fay <no.email@at.all>
2017-05-14 22:40:39 +0200

    Minor layout fixes; Updated ext libs;

--- ext/iconv/Application.mk
+++ ext/iconv/Application.mk
@@ -1,5 +1,5 @@
 APP_MODULES := iconv 
-APP_ABI := arm64-v8a
+APP_ABI := armeabi-v7a arm64-v8a x86 x86_64
 APP_PLATFORM := android-21
 PROJECT_PATH_BASE := q:/Dev/VCMI/vcmi-android
 APP_OPTIM := release
