0b997f4c068800074cc30ffe7c40214b10bc205c
Fay <no.email@at.all>
2017-05-21 11:16:35 +0200

    Updated build setup to allow defs.gradle directly use vcmiconf info;

--- vcmiconf.py
+++ vcmiconf.py
@@ -2,16 +2,9 @@ import sys
 import json
 import vcmiutil
 
-def buildAbisGradleArray():
-	return "[\"" + "\", \"".join(config["abis"].split(" ")) + "\"]"
-
 def updateProjectProps():
 	replacements = [ vcmiutil.ReplacementEntry("PROJECT_PATH_BASE", "PROJECT_PATH_BASE = " + config["projectRoot"]) ]	
 	vcmiutil.fixFile("./project/gradle.properties", replacements, False)
-	
-	replacements = [ vcmiutil.ReplacementEntry("VCMI_ABIS", "\tVCMI_ABIS = " + buildAbisGradleArray()),
-		vcmiutil.ReplacementEntry("VCMI_PATH_BOOST", "\tVCMI_PATH_BOOST = \"${VCMI_PATH_EXT}/boost/" + config["boostFolderName"] + "\"")	]
-	vcmiutil.fixFile("./project/defs.gradle", replacements, False)
 	
 config = []
 with open("./vcmiconf.json", "r") as confFile:
