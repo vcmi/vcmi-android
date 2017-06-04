c99779c1da3cabae68b4380d9281cc228cd686e7
Fay <no.email@at.all>
2017-06-04 18:25:58 +0200

    Updated to current vcmi develop;

--- project/vcmi-app/src/main/java/eu/vcmi/vcmi/util/GeneratedVersion.java
+++ project/vcmi-app/src/main/java/eu/vcmi/vcmi/util/GeneratedVersion.java
@@ -5,5 +5,5 @@ package eu.vcmi.vcmi.util;
  */
 public class GeneratedVersion
 {
-    public static final String VCMI_VERSION = "0.99.0 (b5daa24982b1755d83b0cf5bc4afdee723beea74)";
+    public static final String VCMI_VERSION = "0.99.0 (d84f61fc96ae502a6fa4fe12ca54619f1474e44a)";
 }
