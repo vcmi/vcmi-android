b1c78db2646832e0657fd4edf02ff57998ebb974
Andrii Danylchenko <danilchenko.andrij@gmail.com>
2021-10-16 19:45:43 +0300

    Uppload android artifact

--- .github/CI/upload_package.sh
+++ .github/CI/upload_package.sh
@@ -1,11 +1,21 @@
 #!/bin/sh
-touch /tmp/deploy_rsa
+if [ -z "$DEPLOY_RSA" ];
+then
+	# Due to security measures travis not expose encryption keys for PR from forks
+	echo "Build generation is skipped for forks"
+	exit 0
+fi
+
+echo "$DEPLOY_RSA" > /tmp/deploy_rsa
 chmod 600 /tmp/deploy_rsa
-openssl aes-256-cbc -K $encrypted_a9294e222946_key -iv $encrypted_a9294e222946_iv -in $TRAVIS_BUILD_DIR/CI/deploy_rsa_android.enc -out /tmp/deploy_rsa -d
+
 eval "$(ssh-agent -s)"
 ssh-add /tmp/deploy_rsa
 
-PACKAGE_FULL_PATH=$(find $TRAVIS_BUILD_DIR -type f -name "${TRAVIS_JOB_ID}-vcmi-*.apk")
+PACKAGE_FULL_PATH=$(find $GITHUB_WORKSPACE/project/vcmi-app/build/outputs -type f -name "*-vcmi-*.apk")
+echo "PACKAGE_FULL_PATH = $PACKAGE_FULL_PATH"
 VCMI_PACKAGE_FILE_NAME=${PACKAGE_FULL_PATH##*/}
+echo "VCMI_PACKAGE_FILE_NAME = $VCMI_PACKAGE_FILE_NAME"
 
 sftp -r -o StrictHostKeyChecking=no travis-android@beholder.vcmi.eu <<< "put $PACKAGE_FULL_PATH /incoming/$VCMI_PACKAGE_FILE_NAME"
+
