64e299ebf823986b3c69db963622158d2f65a9b5
Konstantin <ria.freelander@gmail.com>
2022-05-29 14:17:10 +0300

    fix zlibv2

--- project/vcmi-app/cmake-scripts/ext-libs.cmake
+++ project/vcmi-app/cmake-scripts/ext-libs.cmake
@@ -37,7 +37,7 @@ function(build_minizip)
 	set(minizippath ${VCMI_PATH_VCMI}/lib/minizip)
 	add_library(minizip SHARED ${minizippath}/zip.c ${minizippath}/unzip.c ${minizippath}/ioapi.c)
 	set_target_properties(minizip PROPERTIES LINKER_LANGUAGE C)
-	target_link_libraries(minizip ZLIB::ZLIP)
+	target_link_libraries(minizip ZLIB::ZLIB)
 endfunction(build_minizip)
 
 function(build_boost)
